002964738d85c8c46e1ffbed5b845d10c34960c0
IlyaKrn <ilyakornoukhov@gmail.com>
2023-11-11 00:48:54 +0300

    init

---
+++ src/main/java/com/studentscheduleapp/databasetokenservice/DatabaseTokenServiceApplication.java
@@ -0,0 +1,13 @@
+package com.studentscheduleapp.databasetokenservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseTokenServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseTokenServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentscheduleapp/databasetokenservice/DatabaseTokenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentscheduleapp.databasetokenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseTokenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
