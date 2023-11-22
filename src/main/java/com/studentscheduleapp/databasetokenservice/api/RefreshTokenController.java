855e87d9269f7184d283e3b81aee32b452da60ec
IlyaKrn <ilyakornoukhov@gmail.com>
2023-11-22 20:07:43 +0300

    logs

--- src/main/java/com/studentscheduleapp/databasetokenservice/api/RefreshTokenController.java
+++ src/main/java/com/studentscheduleapp/databasetokenservice/api/RefreshTokenController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("api/refresh")
@@ -19,16 +20,31 @@ public class RefreshTokenController {
     @GetMapping("email/{email}")
     public ResponseEntity<String> getById(@PathVariable("email") String email){
         RefreshToken rt = refreshTokenRepository.findById(email).orElse(null);
-        return rt != null ? ResponseEntity.ok(rt.getToken()) : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        if (rt == null){
+            Logger.getGlobal().info("get refresh token failed: not found token for email " + email);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        Logger.getGlobal().info("get refresh token for email " + email + " success ");
+        return ResponseEntity.ok(rt.getToken());
     }
     @PostMapping("save")
     public ResponseEntity<Void> save(@RequestBody RefreshToken data){
+        if (data.getEmail() == null || data.getEmail().isEmpty()){
+            Logger.getGlobal().info("bad request: email is null or empty");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        if (data.getToken() == null || data.getToken().isEmpty()){
+            Logger.getGlobal().info("bad request: token is null or empty");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
         refreshTokenRepository.save(data);
+        Logger.getGlobal().info("save refresh token for email " + data.getEmail() + " success ");
         return ResponseEntity.ok().build();
     }
     @DeleteMapping("delete/{email}")
     public ResponseEntity<List<Void>> deleteById(@PathVariable("email") String email){
         refreshTokenRepository.deleteById(email);
+        Logger.getGlobal().info("save refresh token for email " + email + " success ");
         return ResponseEntity.ok().build();
     }
 }
