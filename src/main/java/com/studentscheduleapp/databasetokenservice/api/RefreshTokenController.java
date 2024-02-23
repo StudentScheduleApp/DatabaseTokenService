96ed5081d326dc45fc84b6551d2f35f3b6aadc68
IlyaKrn <ilyakornoukhov@gmail.com>
2024-02-24 02:24:17 +0300

    log4j

--- src/main/java/com/studentscheduleapp/databasetokenservice/api/RefreshTokenController.java
+++ src/main/java/com/studentscheduleapp/databasetokenservice/api/RefreshTokenController.java
@@ -3,47 +3,50 @@ package com.studentscheduleapp.databasetokenservice.api;
 import com.studentscheduleapp.databasetokenservice.data.repositories.RefreshTokenRepository;
 import com.studentscheduleapp.databasetokenservice.data.tablemodels.RefreshToken;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.logging.Logger;
-
 @RestController
 public class RefreshTokenController {
 
     @Autowired
     private RefreshTokenRepository refreshTokenRepository;
+    private static final Logger log = LogManager.getLogger(RefreshTokenController.class);
+
 
     @GetMapping("${mapping.refresh.getByEmail}/{email}")
     public ResponseEntity<String> getById(@PathVariable("email") String email){
         RefreshToken rt = refreshTokenRepository.findById(email).orElse(null);
-        if (rt == null){
-            Logger.getGlobal().info("get refresh token failed: not found token for email " + email);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-        Logger.getGlobal().info("get refresh token for email " + email + " success ");
-        return ResponseEntity.ok(rt.getToken());
+        log.info("get refreshToken with email: " + email);
+        return ResponseEntity.ok(rt == null ? null : rt.getToken());
     }
     @PostMapping("${mapping.refresh.save}")
     public ResponseEntity<Void> save(@RequestBody RefreshToken data){
         if (data.getEmail() == null || data.getEmail().isEmpty()){
-            Logger.getGlobal().info("bad request: email is null or empty");
+            log.info("bad request: refreshToken email is null or empty");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
         if (data.getToken() == null || data.getToken().isEmpty()){
-            Logger.getGlobal().info("bad request: token is null or empty");
+            log.info("bad request: refreshToken token is null or empty");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-        refreshTokenRepository.save(data);
-        Logger.getGlobal().info("save refresh token for email " + data.getEmail() + " success ");
+        RefreshToken d = refreshTokenRepository.save(data);
+        log.info("save refreshToken with email: " + d.getEmail());
         return ResponseEntity.ok().build();
     }
     @DeleteMapping("${mapping.refresh.delete}/{email}")
     public ResponseEntity<List<Void>> deleteById(@PathVariable("email") String email){
-        refreshTokenRepository.deleteById(email);
-        Logger.getGlobal().info("delete refresh token for email " + email + " success ");
+        try {
+            refreshTokenRepository.deleteById(email);
+            log.info("delete refreshToken with email: " + email);
+        } catch (EmptyResultDataAccessException e){
+            log.warn("delete refreshToken with email: " + email + " failed: entity not exists");
+        }
         return ResponseEntity.ok().build();
     }
 }
