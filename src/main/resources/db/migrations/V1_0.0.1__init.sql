46a35a242d33f4478b42f29f43cb3569c8a59436
IlyaKrn <ilyakornoukhov@gmail.com>
2024-05-10 14:22:53 +0300

    0.1.1-BETA

--- src/main/resources/db/migrations/V1_0.0.1__init.sql
+++ src/main/resources/db/migrations/V1_0.0.1__init.sql
@@ -1,8 +1,4 @@
--- Table: public.refresh_tokens
-
--- DROP TABLE IF EXISTS public.refresh_tokens;
-
-CREATE TABLE IF NOT EXISTS public.refresh_tokens
+CREATE TABLE IF NOT EXISTS refresh_tokens
 (
     email character varying(255) COLLATE pg_catalog."default" NOT NULL,
     token character varying(255) COLLATE pg_catalog."default" NOT NULL,
@@ -10,6 +6,3 @@ CREATE TABLE IF NOT EXISTS public.refresh_tokens
 )
 
 TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.refresh_tokens
-    OWNER to postgres;
